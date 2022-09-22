98bd439ad3cfc97d82fa4229aaa5a593f66ba8af
rak <raks.sail@gmail.com>
2022-09-22 17:07:34 -0400

    test proj changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-project';
+  public title: String;
+  constructor() {
+    this.title = 'test-project';
+  }
+  ngOnInit() {
+    this.title = 'tests-projects';
+  }
+  ngOnChanges() {
+    this.title = 'project';
+  }
 }
